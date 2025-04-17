7db5fdeeff1df558e2f1340d8c17dbc172d53186
abdelrahman <bad606609@gmail.com>
2025-04-17 14:46:11 +0200

    create animated splash view body

--- lib/feature/spalsh/presentation/view/widget/splash_view_body.dart
+++ lib/feature/spalsh/presentation/view/widget/splash_view_body.dart
@@ -1,12 +1,62 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:robotics_app/core/utils/app_images.dart';
 
-class SplashViewBody extends StatelessWidget {
-const SplashViewBody({ super.key });
-@override
-Widget build(BuildContext context) {
-return Container(
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
 
-
-);
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
 }
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
+    )..repeat(reverse: true);
+
+    _animation = Tween<double>(begin: 0.9, end: 1.1).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+
+    Timer(const Duration(seconds: 3), () {
+      // Navigator.pushReplacement(
+      //   context,
+      //   MaterialPageRoute(builder: (context) => const HomePage()),
+      // );
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: AnimatedBuilder(
+        animation: _animation,
+        builder: (context, child) {
+          return Transform.scale(
+            scale: _animation.value,
+            child: Image.asset(
+              Assets.imagesLogo,
+              //
+            ),
+          );
+        },
+      ),
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:robotics_app/core/helper/on_generate_routes.dart';
+import 'package:robotics_app/feature/spalsh/presentation/view/splash_view.dart';
 
-void main(){
+void main() {
   runApp(RoboticsApp());
 }
 
 class RoboticsApp extends StatelessWidget {
-const RoboticsApp({ super.key });
-@override
-Widget build(BuildContext context) {
-return MaterialApp(
-
-
-);
-}
+  const RoboticsApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: SplashView.routeName,
+      onGenerateRoute: onGenerateRoutes,
+    );
+  }
 }
